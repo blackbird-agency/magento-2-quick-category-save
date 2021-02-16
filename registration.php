59b117a0d541bdf287d088d825729334243d6f75
Thibaud Ritzenthaler <thibaud@bird.eu>
2021-02-16 12:01:00 +0100

    module init

---
+++ registration.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Blackbird Quick Category Save
+ *
+ * NOTICE OF LICENSE
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to contact@bird.eu so we can send you a copy immediately.
+ *
+ * @category        Blackbird
+ * @package         Blackbird_QuickCategorySave
+ * @copyright       Copyright (c) Blackbird (https://black.bird.eu)
+ * @author          Thibaud Ritzenthaler (hello@bird.eu)
+ * @license         MIT
+ * @support         https://github.com/blackbird-agency/magento-2-quick-category-save/issues/new
+ */
+
+declare(strict_types=1);
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Blackbird_QuickCategorySave',
+    __DIR__
+);
